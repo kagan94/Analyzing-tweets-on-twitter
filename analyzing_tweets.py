d7f64b921868dee93b9b2fd48eb5e373191eb270
DESKTOP-4RT7P4H\Leo <leo.dashko@gmail.com>
2017-04-04 00:08:37 +0300

    Read the tweets by query with limit. Save/Read tweets to/from file.

--- analyzing_tweets.py
+++ analyzing_tweets.py
@@ -1,29 +1,37 @@
 import json
 import tweepy
-from tweepy import OAuthHandler
 
 
-def save_settings(file_name, obj):
+def save_to_file(file_name, obj):
     with open(file_name, 'w') as f:
         f.write(json.dumps(obj))
 
 
-def read_settings(file_name):
+def read_from_file(file_name):
     with open(file_name, 'r') as f:
         obj = json.loads(f.read())
     return obj
 
 
 # save_settings('access.json', access)
-access = read_settings('my_access.json')
+access = read_from_file('my_access.json')
 
-auth = OAuthHandler(access['consumer_key'], access['consumer_secret'])
+auth = tweepy.OAuthHandler(access['consumer_key'], access['consumer_secret'])
 auth.set_access_token(access['access_token'], access['access_secret'])
 api = tweepy.API(auth)
 
-# Read our own timeline
-for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
-    print(status._json)
-    print(status.text)
+# Request to get tweets
+query, max_tweets = 'Tartu', 5
+found_tweets = [status for status in tweepy.Cursor(api.search, q=query, lang='en').items(max_tweets)]
 
+# Save/Read tweets to/from file
+# found_tweets_json = [tweet._json for tweet in found_tweets]
+# save_to_file('tweets.json', found_tweets_json)
+# found_tweets = read_from_file('tweets.json')
+
+for tweet in found_tweets:
+    print(tweet.text, end='\n\n')
+    print('isTruncated: ', tweet.truncated, end='\n\n')
+
+    # print(tweet['text'], end='\n\n')
+    # print('isTruncated: ', tweet['truncated'], end='\n\n')
